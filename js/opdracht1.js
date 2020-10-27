09fd5dbcabf877f7a8abe46f0b52b6f009356d19
Leslie den Adel <99062688@mydavinci.nl>
2020-10-27 14:44:53 +0100

    opdracht 1 en 2 af

---
+++ js/opdracht1.js
@@ -0,0 +1,7 @@
+function helloWorld(number){
+	for (var i = 1; i<=number; i++) {
+		document.write("Hello World!</br>");
+	}
+}
+
+helloWorld(7)
---
+++ js/opdracht3.js
@@ -0,0 +1,10 @@
+var name;
+var age;
+
+function printName(name,age){
+	document.write("Hallo "+name+", je leeftijd is "+age+" jaar");
+}
+
+while (ask==true) {
+	name = prompt("Wat is je naam?\nOf type stop om te stoppen.");
+}
