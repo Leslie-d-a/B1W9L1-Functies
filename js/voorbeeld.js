e2cd0304a659aef148cd470a107236a91cb56a52
leslie <lesloo@protonmail.com>
2020-10-30 13:28:34 +0100

    fibonacci reeks gemaakt

---
+++ js/voorbeeld.js
@@ -0,0 +1,16 @@
+// var hoeveel = prompt("tot welke tafel wil je?");
+// hoeveel = parseInt(hoeveel);
+// console.log(hoeveel)
+
+for(i=1;i<=10;i++){
+    document.write(i)  
+}
+
+
+
+
+
+// for (i = 1; i <= 3; i++) {
+//     for ()
+//     document.write(i);
+// }
