54aba2b94aadad30d0856b24f6970006509b8976
leslie <lesloo@protonmail.com>
2020-10-30 10:54:56 +0100

    opdracht 3 en 4 af

--- js/opdracht4.js
+++ js/opdracht4.js
@@ -1,21 +1,27 @@
-var askNumber = prompt("Voer een nummer van 1 t/m 10 in.");
-var number = parseInt(askNumber);
+var askNumber;
+var number;
+var ask = true;
 
 function schrijfTafels() {
-    if (number >= 1 && number <= 10) {
-        var number1 = 1;
-        for (a = 1; a <= number; a++) {
-            for (var b = 1; b <= 10; b++) {
-                document.write(b + " * " + a + " = " + number1 * b + "</br>")
+    while (ask == true){
+        askNumber = prompt("Voer een nummer van 1 t/m 10 in.");
+        number = parseInt(askNumber);
+        if (number >= 1 && number <= 10) {
+            ask = false
+            var number1 = 1;
+            for (a = 1; a <= number; a++) {
+                for (var b = 1; b <= 10; b++) {
+                    document.write(b + " * " + a + " = " + number1 * b + "</br>")
+                }
+                number1++
+                document.write("<br>");
             }
-            number1++
-            document.write("<br>");
-        }
-    } else if (typeof number === 'NaN') {
-        alert("U dient een getal in te voeren.");
-    } else if (number <=0 || number >= 11){
-        alert("Bereik van getal moet zijn: 1 t/m 10.");
+        } else if (Number.isNaN(number)) {
+            alert("U dient een getal in te voeren.");
+        } else if (number <=0 || number >= 11){
+            alert("Bereik van getal moet zijn: 1 t/m 10.");
+        }  
     }
+    
 }
-
 schrijfTafels()
